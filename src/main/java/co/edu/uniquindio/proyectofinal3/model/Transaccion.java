56956920e7572a106101e6b35064fd1ae748a580
Carlos Colorado <cscalomejor214@gmail.com>
2024-09-27 21:25:07 -0500

    .

--- src/main/java/co/edu/uniquindio/proyectofinal3/model/Transaccion.java
+++ src/main/java/co/edu/uniquindio/proyectofinal3/model/Transaccion.java
@@ -4,11 +4,20 @@ public class Transaccion {
     String idTransaccion;
     String fecha;
     String tipoTransaccion;
+    String monto;
+    String cuentaOrigen;
+    String cuentaDestino;
+    String descripción;
 
-    public Transaccion(String idTransaccion, String fecha, String tipoTransaccion) {
+
+    public Transaccion(String idTransaccion, String fecha, String tipoTransaccion, String monto,String cuentaOrigen, String cuentaDestino, String descripción) {
         this.idTransaccion = idTransaccion;
         this.fecha = fecha;
         this.tipoTransaccion = tipoTransaccion;
+        this.monto= monto;
+        this.cuentaOrigen = cuentaOrigen;
+        this.cuentaDestino = cuentaDestino;
+        this.descripción= descripción;
     }
 
     public String getIdTransaccion() {
@@ -33,5 +42,37 @@ public class Transaccion {
 
     public void setTipoTransaccion(String tipoTransaccion) {
         this.tipoTransaccion = tipoTransaccion;
+    }
+
+    public String getMonto() {
+        return monto;
+    }
+
+    public void setMonto(String monto) {
+        this.monto = monto;
+    }
+
+    public String getCuentaOrigen() {
+        return cuentaOrigen;
+    }
+
+    public void setCuentaOrigen(String cuentaOrigen) {
+        this.cuentaOrigen = cuentaOrigen;
+    }
+
+    public String getCuentaDestino() {
+        return cuentaDestino;
+    }
+
+    public void setCuentaDestino(String cuentaDestino) {
+        this.cuentaDestino = cuentaDestino;
+    }
+
+    public String getDescripción() {
+        return descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
     }
 }
