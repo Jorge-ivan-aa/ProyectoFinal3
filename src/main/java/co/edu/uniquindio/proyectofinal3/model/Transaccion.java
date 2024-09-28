3cb8b149ac62e1191f27e7e32a622c0b8363e54f
Carlos Colorado <cscalomejor214@gmail.com>
2024-09-27 20:31:37 -0500

    .
    .

--- src/main/java/co/edu/uniquindio/proyectofinal3/model/Transaccion.java
+++ src/main/java/co/edu/uniquindio/proyectofinal3/model/Transaccion.java
@@ -5,4 +5,33 @@ public class Transaccion {
     String fecha;
     String tipoTransaccion;
 
+    public Transaccion(String idTransaccion, String fecha, String tipoTransaccion) {
+        this.idTransaccion = idTransaccion;
+        this.fecha = fecha;
+        this.tipoTransaccion = tipoTransaccion;
+    }
+
+    public String getIdTransaccion() {
+        return idTransaccion;
+    }
+
+    public void setIdTransaccion(String idTransaccion) {
+        this.idTransaccion = idTransaccion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipoTransaccion() {
+        return tipoTransaccion;
+    }
+
+    public void setTipoTransaccion(String tipoTransaccion) {
+        this.tipoTransaccion = tipoTransaccion;
+    }
 }
