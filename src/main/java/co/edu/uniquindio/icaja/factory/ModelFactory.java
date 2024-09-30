8d00e56df7e5e41fa90f995515eebe3b55a24b7f
chaos086 <luisa.marinh@uqvirtual.edu.co>
2024-09-29 20:45:14 -0500

    se crean Admin faltantes

--- src/main/java/co/edu/uniquindio/icaja/factory/ModelFactory.java
+++ src/main/java/co/edu/uniquindio/icaja/factory/ModelFactory.java
@@ -27,8 +27,15 @@ public class ModelFactory {
 
     public void generarAdmin() {
         Usuario admin = new Usuario("admin", "1004827105", "jorgity345@gmail.com", "3175083079", "123", "12345", 0.0);
+        Usuario admin2 = new Usuario ("admin", "1004871460", "hernandezluis1185@gmail.com", "3217998752", "2325", "2325", 0.0);
+        Usuario admin3 = new Usuario ("admin", "1034777786", "carloss.coloradoa@uqvirtual.edu.co", "3014828719", "123", "12345", 0.0);
+
         admin.setAdministrador();
+        admin2.setAdministrador();
+        admin3.setAdministrador();
         icaja.addUsuario(admin);
+        icaja.addUsuario(admin2);
+        icaja.addUsuario(admin3);
     }
 
 }
