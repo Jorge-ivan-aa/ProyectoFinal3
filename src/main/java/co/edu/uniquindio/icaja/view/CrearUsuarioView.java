b99d5028982093fdb4337ca8ed514569f3d90899
Carlos Colorado <cscalomejor214@gmail.com>
2024-10-04 20:29:38 -0500

    ahora si sirve

--- src/main/java/co/edu/uniquindio/icaja/view/CrearUsuarioView.java
+++ src/main/java/co/edu/uniquindio/icaja/view/CrearUsuarioView.java
@@ -2,11 +2,15 @@ package co.edu.uniquindio.icaja.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import co.edu.uniquindio.icaja.controller.UsuarioController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 public class CrearUsuarioView {
+    UsuarioController usuarioController= new UsuarioController();
 
     @FXML
     private ResourceBundle resources;
@@ -37,17 +41,42 @@ public class CrearUsuarioView {
 
     @FXML
     void registrarUsuarioAction(ActionEvent event) {
+        String nombre = txtNombreUsuario.getText();
+        String cedula = txtCedulaUsuario.getText();
+        String correo = txtCorreoUsuario.getText();
+        String clave = txtClaveUsuario.getText();
+        String claveTransaccional = txtClaveTranUsuario.getText();
+        String presupuestoMensual = txtPresupuestoUsuario.getText();
+        String telefono = txtTelefonoUsuario.getText();
+
+        if (!Tools.hayCamposVacios(nombre,  cedula,  correo,  telefono,  clave,  claveTransaccional,  presupuestoMensual)) {
+            String resultado = usuarioController.crearUsuario(nombre,  cedula,  correo,  telefono,  clave,  claveTransaccional, Double.parseDouble(presupuestoMensual));
+            Tools.mostrarMensaje("Información", null, resultado, Alert.AlertType.INFORMATION);
+        } else {
+            Tools.mostrarMensaje("Error", null, "Hay campos vacíos", Alert.AlertType.ERROR);
+
+        }
+        Tools.limpiarCampos(txtCedulaUsuario,
+                txtNombreUsuario,
+                txtCorreoUsuario,
+                txtTelefonoUsuario,
+                txtClaveUsuario,
+                txtClaveTranUsuario,
+                txtPresupuestoUsuario);
 
     }
 
     @FXML
     void volverAction(ActionEvent event) {
+        Tools.ventanaEmergente("login.fxml", "ICaja :)", "styles/main.css");
+
 
     }
 
+
     @FXML
     void initialize() {
-        
+
 
     }
 
