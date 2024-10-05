06196e3dbabb868a418ca1bf17f58ca7ceb5b2fe
Carlos Colorado <cscalomejor214@gmail.com>
2024-10-04 20:16:22 -0500

    interfaz registro de usuario
    interfaz junto con el esqueleto en el view, no hace nada aún y los colores debemos cambiarlos pa q combinen

--- src/main/java/co/edu/uniquindio/icaja/view/CrearUsuarioView.java
+++ src/main/java/co/edu/uniquindio/icaja/view/CrearUsuarioView.java
@@ -1,4 +1,54 @@
 package co.edu.uniquindio.icaja.view;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class CrearUsuarioView {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtCedulaUsuario;
+
+    @FXML
+    private TextField txtClaveTranUsuario;
+
+    @FXML
+    private TextField txtClaveUsuario;
+
+    @FXML
+    private TextField txtCorreoUsuario;
+
+    @FXML
+    private TextField txtNombreUsuario;
+
+    @FXML
+    private TextField txtPresupuestoUsuario;
+
+    @FXML
+    private TextField txtTelefonoUsuario;
+
+    @FXML
+    void registrarUsuarioAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void volverAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        
+
+    }
+
 }
