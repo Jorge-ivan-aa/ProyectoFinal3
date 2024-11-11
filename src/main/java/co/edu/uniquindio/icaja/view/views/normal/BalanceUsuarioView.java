ec71b90607871d920afe71e9d9683c1873722141
Carlos Colorado <cscalomejor214@gmail.com>
2024-11-11 15:45:24 -0500

    ajustes en la interfaz de balanceUsuario

--- src/main/java/co/edu/uniquindio/icaja/view/views/normal/BalanceUsuarioView.java
+++ src/main/java/co/edu/uniquindio/icaja/view/views/normal/BalanceUsuarioView.java
@@ -1,14 +1,16 @@
 package co.edu.uniquindio.icaja.view.views.normal;
 
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXListView;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import io.github.palexdev.materialfx.controls.MFXListView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
 
 public class BalanceUsuarioView {
 
@@ -19,10 +21,19 @@ public class BalanceUsuarioView {
     private URL location;
 
     @FXML
+    private MFXComboBox<?> cbCategoriasBalance;
+
+    @FXML
     private MFXListView<?> lvListaPresupuestosEstadisticas;
 
     @FXML
     private MFXListView<?> lvListaTransaccionesBalance;
+
+    @FXML
+    private Pane panelBalanceUsuario1;
+
+    @FXML
+    private Pane panelBalanceUsuario2;
 
     @FXML
     private RadioButton rbGastosBalance;
@@ -34,12 +45,61 @@ public class BalanceUsuarioView {
     private RadioButton rbTodosBalance;
 
     @FXML
+    private TableColumn<?, ?> tcCategoriaBalance;
+
+    @FXML
+    private TableColumn<?, ?> tcIdBalance;
+
+    @FXML
+    private TableColumn<?, ?> tcMontoAsignadoBalance;
+
+    @FXML
+    private TableColumn<?, ?> tcMontoGastadoBalance;
+
+    @FXML
+    private TableColumn<?, ?> tcNombreBalance;
+
+    @FXML
+    private TableView<?> tvListaBalances;
+
+    @FXML
+    private MFXTextField txtMontoBalance;
+
+    @FXML
+    private MFXTextField txtNombreBalance;
+
+    @FXML
+    void actualizarBalanceAction(ActionEvent event) {
+
+    }
+
+    @FXML
     void ajustarPresupuestoBalanceAction(ActionEvent event) {
 
     }
 
     @FXML
+    void crearBalanceAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarBalanceAction(ActionEvent event) {
+
+    }
+
+    @FXML
     void generarReporteFinancieroBalanceAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void limpiarCamposAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void volverAction(ActionEvent event) {
 
     }
 
