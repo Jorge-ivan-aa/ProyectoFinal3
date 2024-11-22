c3629b9e05bbdd8706ddfe8c8d589bd150ebdaf5
Carlos Colorado <cscalomejor214@gmail.com>
2024-11-21 20:46:04 -0500

    .

--- src/main/java/co/edu/uniquindio/icaja/view/views/normal/PerfilUsuarioView.java
+++ src/main/java/co/edu/uniquindio/icaja/view/views/normal/PerfilUsuarioView.java
@@ -108,7 +108,7 @@ public class PerfilUsuarioView {
             UsuarioDto usuarioDto = new UsuarioDto(null,null,  null,  null,  null, confirmarContra ,""  );
             try {
                 usuarioController.actualizar(usuarioDto);
-                String msj = "Se ha actualizado el usuario correctamente";
+                String msj = "Se ha actualizado la contraseña del usuario correctamente";
                 ViewTools.mostrarMensaje("Información", null, msj, Alert.AlertType.INFORMATION);
 
             } catch (ElementoNoExiste e) {
@@ -126,8 +126,21 @@ public class PerfilUsuarioView {
     void configurarTranAction(ActionEvent event) {
         String nuevaContraTran= txtNuevaContrasenaTran.getText();
         String configurarContraTran = txtConfirmarContrasenaTran.getText();
-        if(nuevaContraTran == configurarContraTran){
+        if (nuevaContraTran== configurarContraTran){
+            UsuarioDto usuarioDto = new UsuarioDto(null,null,  null,  null,  null, "" ,configurarContraTran  );
+            try {
+                usuarioController.actualizar(usuarioDto);
+                String msj = "Se ha actualizado la contraseña transaccional del usuario ";
+                ViewTools.mostrarMensaje("Información", null, msj, Alert.AlertType.INFORMATION);
 
+            } catch (ElementoNoExiste e) {
+                ViewTools.mostrarMensaje("Error", null, e.getMessage(), Alert.AlertType.ERROR);
+            }
+            String msj = "Se ha actualizado la contraseña transaccional correctamente";
+            ViewTools.mostrarMensaje("Información", null, msj, Alert.AlertType.INFORMATION);
+        }else{
+            String msj = "No se pudo actualizar la contraseña transaccional correctamente";
+            ViewTools.mostrarMensaje("Información", null, msj, Alert.AlertType.INFORMATION);
         }
 
     }
