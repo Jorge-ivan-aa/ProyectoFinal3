c6a7e497e074ffba0b7265c0211e944825df13d8
Carlos Colorado <cscalomejor214@gmail.com>
2024-11-21 21:04:22 -0500

    .

--- src/main/java/co/edu/uniquindio/icaja/view/views/normal/PrincipalUsuarioView.java
+++ src/main/java/co/edu/uniquindio/icaja/view/views/normal/PrincipalUsuarioView.java
@@ -120,7 +120,7 @@ public class PrincipalUsuarioView {
 
         ViewTools.cambiarPantalla(panelCharlarIA,0.225, panelTransaccionUsuario);
         //Agregar un mensaje inicial por parte del chatbot
-        String mensajeInicial = "¡Hola! Soy tu asistente virtual, digita: 1. Para generalidades de la app. Digita: 2. Para estrategias de ahorros";
+        String mensajeInicial = "¡Hola! Soy tu asistente Icaja virtual, digita: 1. Para generalidades de la app. Digita: 2. Para estrategias de ahorros";
         AnchorPane userMessage2 = crearMensaje(mensajeInicial, false);
         lvListaChatConIA.getItems().add(userMessage2);
     }
